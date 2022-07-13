b2a58f898102d1d57dd40ed8836dea337fa08b6f
Ricardo Rivera Pérez <43622894+Ricardo55010@users.noreply.github.com>
2022-07-13 14:29:55 -0500

    scanner that reads an int, string and double

---
+++ challenges/scanner.java
@@ -0,0 +1,17 @@
+package challenges;
+import java.util.Scanner;
+
+public class scanner {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int i = scan.nextInt();
+        double d = scan.nextDouble();
+        String s = scan.nextLine();
+        s = scan.nextLine();//we read to two times nextline thanks to the cache that is stored after using nextInt
+        scan.close();
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+    }
+}
